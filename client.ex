a4af8a3ea70cbdc596745405401106d0e6c76c03
Andrea Leopardi <an.leopardi@gmail.com>
2019-03-28 18:26:26 +0100

    Don't use a GenServer in the client code

--- client.ex
+++ client.ex
@@ -1,8 +1,4 @@
 defmodule Chat.Client do
-  use GenServer
-
-  require Logger
-
   def gets_loop(nickname) do
     message = String.trim(IO.gets("#{nickname}: "))
     message = %{"kind" => "broadcast", "message" => message, "nickname" => nickname}
@@ -21,42 +17,50 @@ defmodule Chat.Client do
           {String.to_charlist(address), String.to_integer(port)}
       end
 
-    {:ok, _} = GenServer.start_link(__MODULE__, {address, port}, name: :client)
+    parent = self()
+    loop_pid = spawn_link(fn -> process_loop(parent, address, port) end)
+
+    receive do
+      {:ready, ^loop_pid} -> :ok
+    end
+
+    Process.register(loop_pid, :client)
 
     nickname = String.trim(IO.gets("Nickname: "))
     gets_loop(nickname)
   end
 
-  def init({address, port}) do
+  defp process_loop(parent, address, port) do
     {:ok, socket} = :gen_tcp.connect(address, port, [:binary, packet: 2, active: true])
-    Logger.info("Connected to #{address}:#{port}")
-    {:ok, socket}
+    send(parent, {:ready, self()})
+    process_loop(socket)
   end
 
-  def handle_info({:send_to_server, message}, socket) do
-    payload = Jason.encode!(message)
-    :ok = :gen_tcp.send(socket, payload)
-    {:noreply, socket}
-  end
+  defp process_loop(socket) do
+    receive do
+      {:send_to_server, message} ->
+        payload = Jason.encode!(message)
+        :ok = :gen_tcp.send(socket, payload)
+        process_loop(socket)
 
-  def handle_info({:tcp, socket, data}, socket) do
-    case Jason.decode(data) do
-      {:ok, %{"kind" => "broadcast", "nickname" => broadcaster_nickname, "message" => message}} ->
-        IO.puts("#{broadcaster_nickname}: #{message}")
+      {:tcp, ^socket, data} ->
+        case Jason.decode(data) do
+          {:ok,
+           %{"kind" => "broadcast", "nickname" => broadcaster_nickname, "message" => message}} ->
+            IO.puts("#{broadcaster_nickname}: #{message}")
 
-      {:error, reason} ->
-        IO.puts("ERROR: error decoding JSON: #{inspect(reason)}")
+          {:error, reason} ->
+            IO.puts("ERROR: error decoding JSON: #{inspect(reason)}")
+        end
+
+        process_loop(socket)
+
+      {:tcp_closed, ^socket} ->
+        raise "TCP connection was closed"
+
+      {:tcp_error, ^socket, reason} ->
+        raise "TCP connection error: #{:inet.format_error(reason)}"
     end
-
-    {:noreply, socket}
-  end
-
-  def handle_info({:tcp_closed, socket}, socket) do
-    raise "TCP connection was closed"
-  end
-
-  def handle_info({:tcp_error, socket, reason}, socket) do
-    raise "TCP connection error: #{:inet.format_error(reason)}"
   end
 end
 
