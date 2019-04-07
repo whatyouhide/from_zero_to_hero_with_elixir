386b2b4e192c05cb4a689e1ec5097e9f8aec9747
Andrea Leopardi <an.leopardi@gmail.com>
2019-04-07 13:01:23 +0200

    Require Elixir ~> 1.8 instead of master

--- chat/mix.exs
+++ chat/mix.exs
@@ -5,7 +5,7 @@ defmodule Chat.MixProject do
     [
       app: :chat,
       version: "0.1.0",
-      elixir: "~> 1.9-dev",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
