cbe46aead79456ca1aed90a8e8b39a558fdf0f99
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-04-10 11:54:36 +0200

    Fix basics boilerplate and solutions

--- exercises/lib/exercises/basics.ex
+++ exercises/lib/exercises/basics.ex
@@ -2,39 +2,21 @@ defmodule Exercises.Basics do
   @doc """
   Sums the value of all integer values in a list.
   """
-  def sum([]) do
-    0
-  end
-
-  def sum([head | tail]) do
-    head + sum(tail)
+  def sum(_list) do
+    raise "not implemented yet"
   end
 
   @doc """
   Takes the first the `count` elements of a list and returns them.
   """
-  def take([head | tail], count) when count > 0 do
-    [head | take(tail, count - 1)]
-  end
-
   def take(_list, _count) do
-    []
+    raise "not implemented yet"
   end
 
   @doc """
   Returns the minimum element of the list.
   """
-  def min([head]) do
-    head
-  end
-
-  def min([head | tail]) do
-    min_tail = min(tail)
-
-    if head < min_tail do
-      head
-    else
-      min_tail
-    end
+  def min(_list) do
+    raise "not implemented yet"
   end
 end
